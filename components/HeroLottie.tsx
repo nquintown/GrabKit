f38c2154f09b1e4cc50c2d621ee2d2c844418c67
ScarletUX <quintown.n@gmail.com>
2026-04-30 14:22:11 +0200

    resize

--- components/HeroLottie.tsx
+++ components/HeroLottie.tsx
@@ -7,7 +7,7 @@ const Lottie = dynamic(() => import('lottie-react'), { ssr: false })
 
 export default function HeroLottie() {
   return (
-    <div className="mx-auto w-48 sm:w-56" style={{ mixBlendMode: 'multiply' }}>
+    <div className="mx-auto w-64 sm:w-80" style={{ mixBlendMode: 'multiply' }}>
       <Lottie
         animationData={animationData}
         loop
